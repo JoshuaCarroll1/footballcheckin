9faa809db244066b8014516933b4bb9667debe58
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-11-21 10:33:21 +0000

    Update allowed hosts for Heroku

--- football_checkup/settings.py
+++ football_checkup/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['8000-joshuacarro-footballche-odbqo7qhqiz.ws-eu106.gitpod.io', 'footballcheckup-ae432318f52b.herokuapp.com/']  # noqa
+ALLOWED_HOSTS = ['8000-joshuacarro-footballche-odbqo7qhqiz.ws-eu106.gitpod.io', 'footballcheckup-ae432318f52b.herokuapp.com']  # noqa
 
 
 # Application definition
