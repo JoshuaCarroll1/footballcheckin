77368d6f6fc4fb2258f904c3120a56ca19241692
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-12-05 12:36:46 +0000

    Add view to edit existing matches

--- matches/views.py
+++ matches/views.py
@@ -38,3 +38,27 @@ def add_match(request):
     return render(request, template, context)
 
 
+def edit_match(request, id):
+    """
+    Function to edit a new match - if superuser
+    """
+    if not request.user.is_superuser:
+        messages.error(request, "Not authorised. Please try again.")
+        return redirect(reverse(get_matches))
+    # is superuser - proceed
+    match = get_object_or_404(Match, id=id)
+    if request.method == "POST":
+        form = MatchForm(request.POST or None, request.FILES, instance=match)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Match updated!")
+            return redirect(reverse(get_matches))
+        messages.error(request, "Error - please try again")
+    form = MatchForm(instance=match)
+    template = "matches/edit_match.html"
+    context = {
+        "form": form,
+        "match": match,
+    }
+    return render(request, template, context)
+
